c32b9ce25ca8f381c3a9d9bf9deb87f9618bc35c
i3ppwn <i3ppwn@gmail.com>
2013-11-04 10:14:40 -0800

    Update device_types.h
    Devices are now updqted to the latest

--- device_types.h
+++ device_types.h
@@ -16,12 +16,20 @@ static struct device_type_t device_types[] = {
 	{"iPad1,1", "iPad"},
 	{"iPhone3,1", "iPhone 4 (GSM)"},
 	{"iPod4,1", "iPod Touch (4G)"},
+	{"iPod5,1","Pod Touvh (5G)"}
 	{"AppleTV2,1", "AppleTV (2G)"},
 	{"iPhone3,1", "iPhone 4 (CDMA)"},
 	{"iPad2,1", "iPad 2"},
 	{"iPad2,2", "iPad 2 (GSM)"},
 	{"iPad2,3", "iPad 2 (CDMA"},
+	{"iPad3,1", "iPad 2"},
+	{"iPad3,2", "iPad 2 (GSM)"},
+	{"iPad3,3", "iPad 2 (CDMA"},
+	{"iPad4,1", "iPad 2"},
+	{"iPad4,2", "iPad 2 (GSM)"},
+	{"iPad4,3", "iPad 2 (CDMA"},
 	{"iPhone4,1", "iPhone 4S"},
+	{"iPhone5,1", "iPhone 5"}
 	{NULL, NULL}
 };
 
